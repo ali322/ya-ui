eb0d4e8d3ad039ab643f37f99896a5c0b4bbb7b7
alichen <ali322@gmail.com>
2015-08-01 15:37:39 +0800

    change exports

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ let GoTop = require('./component/gotop.jsx'),
     Loading = require("./component/loading.jsx"),
     Selected = require("./component/selected.jsx");
 
+let loadingAction = require("./action/loading"),
+    dialogAction = require("./action/dialog"),
+    messagerAction = require("./action/messager");
+
+
 module.exports = {
-    GoTop,
-    Dialog,
-    Messager,
-    Loading,
-    Selected
+    component:{
+        GoTop,
+        Dialog,
+        Messager,
+        Loading,
+        Selected
+    },
+    action:{
+        loadingAction,
+        dialogAction,
+        messagerAction
+    }
 }
