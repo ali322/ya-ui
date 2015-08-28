65c58edd97505bbcc26e4366d76d65af1901f562
alichen <ali322@gmail.com>
2015-08-28 16:15:35 +0800

    change dom

--- src/util/dom.js
+++ src/util/dom.js
@@ -1,11 +1,17 @@
 'use strict'
 module.exports = {
-    scrollTop(){
+    scrollTop(value){
         let isCSS1Compat = (document.compatMode === 'CSS1Compat');
         let supportPageOffset = window.pageYOffset !== undefined;
         let scrollTop = supportPageOffset ? window.pageYOffset : 
                         isCSS1Compat? document.documentElement.scrollTop:
                         document.body.scrollTop;
+        let scrollLeft = supportPageOffset ? window.pageXOffset : 
+                        isCSS1Compat? document.documentElement.scrollLeft:
+                        document.body.scrollLeft;
+        if(value !== undefined){
+            window.scrollTo(scrollLeft,value);
+        }
         return scrollTop;
     }
 }
