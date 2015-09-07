6e9676a894f5c652595cd104cf007e0acc47fbb7
alichen <ali322@gmail.com>
2015-09-07 18:42:44 +0800

    change util

--- src/util/dom.js
+++ src/util/dom.js
@@ -1,5 +1,25 @@
 'use strict'
 module.exports = {
+  offset: function(element) {
+        if (element) {
+          var rect = element.getBoundingClientRect();
+          var body = document.body;
+          var clientTop = element.clientTop || body.clientTop || 0;
+          var clientLeft = element.clientLeft || body.clientLeft || 0;
+          var scrollTop = window.pageYOffset || element.scrollTop;
+          var scrollLeft = window.pageXOffset || element.scrollLeft;
+    
+          return {
+            top: rect.top + scrollTop - clientTop,
+            left: rect.left + scrollLeft - clientLeft
+          };
+        }
+        return null;
+    },
+    ownerWindow(element){
+        const doc = (element && element.ownerDocument) || document;
+        return doc.defaultView || doc.parentWindow || window;
+    },
     scrollTop(value){
         let isCSS1Compat = (document.compatMode === 'CSS1Compat');
         let supportPageOffset = window.pageYOffset !== undefined;
