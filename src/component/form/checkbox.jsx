6f0bd145d1e0d2bcdfdc74cb91e9e0d6c29f1e65
alichen <ali322@gmail.com>
2015-10-08 18:48:04 +0800

    fix checkbox

--- src/component/form/checkbox.jsx
+++ src/component/form/checkbox.jsx
@@ -5,21 +5,28 @@ import classNames from "classnames";
 import Icon from "../core/icon.jsx";
 
 class Checkbox extends Component{
+    componentDidUpdate(prevProps,prevState){
+        if(this.props.checked === false && prevProps.checked === true){
+            React.findDOMNode(this.refs.checkInput).checked = false; 
+        }
+    }
     render(){
         const {type,name,checked,onChange} = this.props;
         var checkedIcon = "check",uncheckIcon = "check-empty";
         var checkInput = (
-            <input type="checkbox" onChange={onChange} defaultChecked={checked}/>
+            <input type="checkbox" onChange={onChange} ref="checkInput" defaultChecked={checked}/>
         );
         if(type === "radio"){
             checkedIcon = "dot-circled";
             uncheckIcon = "circle-empty";
-            checkInput = (<input type="radio" name={name} onChange={onChange} defaultChecked={checked}/>);
+            checkInput = (<input type="radio" name={name} onChange={onChange} defaultChecked={checked} 
+            ref="checkInput"/>);
         }
         checkedIcon = this.props.checkedIcon ? this.props.checkedIcon:checkedIcon;
-        uncheckIcon = this.props.uncheckIcon ? this.props.uncheckIcon:uncheckIcon
+        uncheckIcon = this.props.uncheckIcon ? this.props.uncheckIcon:uncheckIcon;
+        const classes = classNames("checkbox",this.props.className);
         return (
-            <div className="checkbox">
+            <div className={classes}>
             {checkInput}
             <Icon icon={checkedIcon} className="checked"/>
             <Icon icon={uncheckIcon} className="unchecked"/>
