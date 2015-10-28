e62486da7af65c7f3190baa56cf2323af7908d58
chenli <ali322@gmail.com>
2015-10-28 12:07:14 +0800

    change alert

--- src/component/alert/alert.jsx
+++ src/component/alert/alert.jsx
@@ -1,15 +1,18 @@
 'use strict'
 import React,{Component} from "react";
 import classNames from "classnames";
+import dom from "../../lib/dom.es6";
 
 class Alert extends Component{
     render(){
-        const classes = classNames("alert",{
+        const classes = classNames("alert-layer",{
             "active":this.props.active
         })
         return (
             <div className={classes}>
+            <div className="alert">
             {this.props.children}
+            </div>
             </div>
         );
     }
