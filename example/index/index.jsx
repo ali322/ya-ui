11435b15f964caf6c64916906db4595cd64f6d3f
chenli <ali322@gmail.com>
2015-12-03 18:08:22 +0800

    fix example index

--- example/index/index.jsx
+++ example/index/index.jsx
@@ -8,11 +8,26 @@ class IndexExample extends Component{
     }
     render(){
         return (
-            <div className="index-content">
-                <ul>
-                    <li><a href="./selected/selected.html">Selected</a></li>
-                    <li><a href="./popup/popup.html">Popup</a></li>
-                </ul>
+            <div className="index-example">
+                <h2>Ya-UI</h2>
+                <table className="md-data-table">
+                    <thead>
+                    <tr>
+                        <th>Component</th>
+                        <th>Example</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Selected</td>
+                        <td><a href="./selected/selected.html">example</a></td>
+                    </tr>
+                    <tr>
+                        <td>Popup</td>
+                        <td><a href="./popup/popup.html">example</a></td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
