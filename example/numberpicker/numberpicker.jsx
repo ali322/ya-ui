acbe5a59e5430ff4265c18e02c47c21b60345e91
chenli <ali322@gmail.com>
2015-12-08 11:40:24 +0800

    add number picker example

---
+++ example/numberpicker/numberpicker.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from "react";
+
+class NumberpickerForm extends Component{
+    render(){
+        return (
+            <div className="numberpicker-example">
+
+            </div>
+        )
+    }
+}
