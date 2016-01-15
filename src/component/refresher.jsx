a0cb8f2930fc6bb3a87a69ae9433699dcf90f5da
chenli <ali322@gmail.com>
2016-01-15 16:41:04 +0800

    change refresher

--- src/component/refresher.jsx
+++ src/component/refresher.jsx
@@ -1,9 +1,29 @@
 'use strict'
 import React from "react";
+import ReactDOM from "react-dom";
 import classNames from "classnames";
-import Icon from "./icon.jsx";
+import dom from "../lib/dom.es6";
 
 class Refresher extends React.Component{
+    componentDidMount(){
+        this.scrollNode = ReactDOM.findDOMNode(this).parentNode
+        // console.log('scrollNode',this.scrollNode)
+        dom.bindEvent(this.scrollNode,"scroll",(e)=>{
+            this.handleScroll(this.scrollNode)
+        })
+    }
+    handleScroll(scrollNode){
+        const {handleRefresh,threshold} = this.props
+        let scrollTop = dom.scrollTop(scrollNode)
+        if((scrollNode.offsetHeight + scrollTop + threshold) >= scrollNode.scrollHeight){
+            handleRefresh()
+        }
+    }
+    componentWillUnmount(){
+        dom.unbindEvent(this.scrollNode,"scroll",(e)=>{
+            this.handleScroll(this.scrollNode)
+        })
+    }
     render(){
         var classes = classNames({
             "refresher":true,
@@ -11,10 +31,15 @@ class Refresher extends React.Component{
         })
         return (
             <div className={classes}>
-                <Icon icon="spin-5" className="animate-spin"/>Loading
+                <span className="iconfont icon-loading animate-spin"></span>正在加载
             </div>
         );
     }
+}
+
+Refresher.defaultProps = {
+    threshold:10,
+    handleRefresh:()=>{}
 }
 
 export default Refresher;
