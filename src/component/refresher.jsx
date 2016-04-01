1bff619883b3bcc356489f39ddbabbf5dd38aa0f
chenli <ali322@gmail.com>
2016-04-01 17:28:36 +0800

    change refresher

--- src/component/refresher.jsx
+++ src/component/refresher.jsx
@@ -3,21 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import classNames from "classnames";
 import dom from "../lib/dom.es6";
+import SmoothScroll from "../lib/dom/smoothscroll.es6";
 
 class Refresher extends React.Component{
-    componentDidMount(){
-        this.scrollNode = this.props.isInnerScroll?ReactDOM.findDOMNode(this).parentNode:window
-        // console.log('scrollNode',this.scrollNode)
-        dom.bindEvent(this.scrollNode,"scroll",(e)=>{
-            this.handleScroll(this.scrollNode)
-        })
+    constructor(props){
+        super(props)
     }
-    handleScroll(scrollNode){
-        const {handleRefresh,threshold} = this.props
-        let scrollTop = dom.scrollTop(scrollNode)
-        if((scrollNode.offsetHeight + scrollTop + threshold) >= scrollNode.scrollHeight){
-            handleRefresh()
-        }
+    componentDidMount(){
+        this.scrollNode = ReactDOM.findDOMNode(this).parentNode
+        dom.bindEvent(this.scrollNode,"scroll",this.handleScroll.bind(this))
     }
     componentWillUnmount(){
         dom.unbindEvent(this.scrollNode,"scroll",(e)=>{
@@ -38,9 +32,9 @@ class Refresher extends React.Component{
 }
 
 Refresher.defaultProps = {
-    threshold:10,
-    isInnerScroll:true,
-    handleRefresh:()=>{}
+    smooth:false,
+    active:false,
+    threshold:10
 }
 
 export default Refresher;
